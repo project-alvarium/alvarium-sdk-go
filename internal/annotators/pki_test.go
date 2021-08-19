8de04fabaa179eb3dea7dd6844723922db0639d0
DyrellC <dyrell.chapman@iota.org>
2021-08-19 11:25:47 -0600

    Update package imports

--- internal/annotators/pki_test.go
+++ internal/annotators/pki_test.go
@@ -16,9 +16,9 @@ package annotators
 import (
 	"context"
 	"encoding/json"
-	"github.com/project-alvarium/alvarium-sdk-go/internal/signprovider/ed25519"
-	"github.com/project-alvarium/alvarium-sdk-go/pkg/config"
-	"github.com/project-alvarium/alvarium-sdk-go/test"
+	"github.com/DyrellC/alvarium-sdk-go/internal/signprovider/ed25519"
+	"github.com/DyrellC/alvarium-sdk-go/pkg/config"
+	"github.com/DyrellC/alvarium-sdk-go/test"
 	"io/ioutil"
 	"testing"
 )
